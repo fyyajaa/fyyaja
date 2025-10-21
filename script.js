060e5ed6d9340c6d082e7c8df57e180ce71db48a
fyyajaa <flyyajaa4@gmail.com>
2025-10-21 13:27:28 +0700

    Add click event listener to greeting button

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih ombol berdasarkan I-nya
+const tombolsapa = ducument.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.addEventListener('click',function() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Halo! terima kasih sudah berkunjung!');
+});
